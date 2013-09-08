a1fa5384259bff5d281fad3f5d15a8223ca8dd9a
Barry G. Becker <barrybecker4@yahoo.com>
2013-09-08 07:26:26 -0700

    Improve rendering for bridge problem.

--- source/com/barrybecker4/puzzle/bridge/ui/BridgeRenderer.java
+++ source/com/barrybecker4/puzzle/bridge/ui/BridgeRenderer.java
@@ -19,11 +19,11 @@ import java.util.List;
 public class BridgeRenderer implements PuzzleRenderer<Bridge> {
 
     public static final int INC = 60;
-    public static final int BRIDGE_WIDTH = 380;
+    public static final int BRIDGE_WIDTH = 260;
 
     private static final int MARGIN = 50;
-    private static final int TEXT_WIDTH = 200;
-    private static final int TEXT_Y = 120;
+    private static final int TEXT_WIDTH = 230;
+    private static final int TEXT_Y = 190;
 
     private static final Font FONT = new Font("Sans Serif", Font.PLAIN, INC/2);
 
@@ -61,7 +61,7 @@ public class BridgeRenderer implements PuzzleRenderer<Bridge> {
 
         g.setColor( Color.darkGray );
         for ( int i = 0; i <= 10; i++ ) {
-            int ypos = 20 + TEXT_Y + i;
+            int ypos = TEXT_Y + i;
             g.drawLine(leftX, ypos, rightX, ypos);
         }
     }
@@ -69,7 +69,18 @@ public class BridgeRenderer implements PuzzleRenderer<Bridge> {
     private void drawPeople(Graphics g, List<Integer> people, int xpos) {
         g.setColor(Color.BLACK);
         g.setFont(FONT);
-        g.drawString(people.toString(), xpos, TEXT_Y);
+        String peopleListString = people.toString();
+        if (people.size() <= 5) {
+            g.drawString(peopleListString, xpos + 10, TEXT_Y);
+        }
+        else {
+            // split into 2 lines for better readability
+            int idx = peopleListString.indexOf(",", 12) + 1;
+            String part1 = peopleListString.substring(0, idx);
+            String part2 = peopleListString.substring(idx);
+            g.drawString(part1, xpos + 10, TEXT_Y);
+            g.drawString(part2, xpos + 10, TEXT_Y + 40);
+        }
     }
 
     private void drawLight(Graphics g, boolean isLightCrossed) {
@@ -79,9 +90,9 @@ public class BridgeRenderer implements PuzzleRenderer<Bridge> {
         int xpos = isLightCrossed ? rightPos : leftPos;
 
         g.setColor(Color.YELLOW);
-        g.fillOval(xpos, TEXT_Y + 40, LIGHT_RADIUS, LIGHT_RADIUS);
+        g.fillOval(xpos, TEXT_Y - 80, LIGHT_RADIUS, LIGHT_RADIUS);
         g.setColor(Color.BLACK);
-        g.drawOval(xpos, TEXT_Y + 40, LIGHT_RADIUS, LIGHT_RADIUS);
+        g.drawOval(xpos, TEXT_Y - 80, LIGHT_RADIUS, LIGHT_RADIUS);
     }
 
 }
