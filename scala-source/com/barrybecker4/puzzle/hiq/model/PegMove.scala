1e80a3a5660b67b63e78862faa726da59af39791
barrybecker4 <barrybecker4@yahoo.com>
2017-01-08 08:28:23 -0800

    Make PegMove an immutable case class.

--- scala-source/com/barrybecker4/puzzle/hiq/model/PegMove.scala
+++ scala-source/com/barrybecker4/puzzle/hiq/model/PegMove.scala
@@ -2,53 +2,32 @@
 package com.barrybecker4.puzzle.hiq.model
 
 import com.barrybecker4.common.geometry.ByteLocation
-import com.barrybecker4.common.geometry.Location
 import com.barrybecker4.puzzle.common.model.Move
 
 /**
   * Definition for a peg jumping another peg.
+  * Create a move object representing a transition on the board.
+  * A naive implementation might use 4 four byte integers to store the from and to values.
+  * This would use 16 bytes of memory per move.
+  * If we do this, we will quickly run out of memory because fo the vast numbers of moves that must be stored.
+  * I will use just 1 byte to store the move information.
+  * All we need to know is the from position (which can be stored in 6 bits) and the to direction (which can be stored in 2 bits)
+  * I know that a jump is always 2 spaces.
   * Immutable.
   * @author Barry Becker
   */
-final class PegMove extends Move {
-  /* the position to move to */
-  private var toPosition: ByteLocation = _
+final case class PegMove(fromPosition: ByteLocation, toPosition: ByteLocation) extends Move {
 
-  /** The position we moved from. */
-  private var fromPosition: ByteLocation = _
-
-  /**
-    * create a move object representing a transition on the board.
-    * A naive implementation might use 4 four byte integers to store the from and to values.
-    * This would use 16 bytes of memory per move.
-    * If we do this, we will quickly run out of memory because fo the vast numbers of moves that must be stored.
-    * I will use just 1 byte to store the move information.
-    * All we need to know is the from position (which can be stored in 6 bits) and the to direction (which can be stored in 2 bits)
-    * I know that a jump is always 2 spaces.
-    */
   def this(fromRow: Byte, fromCol: Byte, destinationRow: Byte, destinationCol: Byte) {
-    this()
-    fromPosition = new ByteLocation(fromRow, fromCol)
-    toPosition = new ByteLocation(destinationRow, destinationCol)
+    this(new ByteLocation(fromRow, fromCol), new ByteLocation(destinationRow, destinationCol))
   }
 
-  def this(fromPosition: Location, destinationPosition: Location) {
-    this()
-    this.fromPosition = fromPosition.copy.asInstanceOf[ByteLocation]
-    this.toPosition = destinationPosition.copy.asInstanceOf[ByteLocation]
-  }
-
-  /**
-    * @return a deep copy.
-    */
-  def copy = new PegMove1(fromPosition, toPosition)
+  /** @return a deep copy. */
+  def copy = PegMove(fromPosition, toPosition)
 
   def getFromRow: Byte = fromPosition.getRow.toByte
-
   def getFromCol: Byte = fromPosition.getCol.toByte
-
   def getToRow: Byte = toPosition.getRow.toByte
-
   def getToCol: Byte = toPosition.getCol.toByte
 
   override def toString: String = {
