9d50340a19facba18b7af052ea254fd291839fa1
barrybecker4 <barrybecker4@yahoo.com>
2017-03-25 05:08:33 -0700

    Porting tantrix code to scala. Avoid new in PathColors constructor. WIP.

--- scala-source/com/barrybecker4/puzzle/tantrix/model/PathColors.scala
+++ scala-source/com/barrybecker4/puzzle/tantrix/model/PathColors.scala
@@ -9,17 +9,14 @@ object PathColor extends Enumeration {
   val RED, GREEN, BLUE, YELLOW, WHITE = Value
 }
 
-/* This should have worked, but didn't
+/* This should have worked, but didn't */
 object PathColors {
-  def PathColors(c1: PathColor, c2: PathColor, c3: PathColor, c4: PathColor, c5: PathColor, c6: PathColor) =
+  def apply(c1: PathColor, c2: PathColor, c3: PathColor, c4: PathColor, c5: PathColor, c6: PathColor) =
     new PathColors(Array(c1, c2, c3, c4, c5, c6))
-}*/
+}
 
 /** The list of 6 edge colors starting from the right side and going counter clockwise. Immutable */
 case class PathColors(colors: Array[PathColor]) {
-
-  def this(c1: PathColor, c2: PathColor, c3: PathColor, c4: PathColor, c5: PathColor, c6: PathColor) =
-    this(Array(c1, c2, c3, c4, c5, c6))
 
   def apply(i: Int): PathColor = colors(i)
 }
