bb6683812fed7152c7e8aee9b44cd1f31f0d720c
barrybecker4 <barrybecker4@yahoo.com>
2017-05-29 19:19:18 -0700

    Start porting common package to scala. Add tests.

---
+++ scala-test/com/barrybecker4/puzzle/common/solver/CountDownLatch1Suite.scala
@@ -0,0 +1,92 @@
+package com.barrybecker4.puzzle.common.solver
+
+import com.barrybecker4.common.concurrency.ThreadUtil
+import org.junit.Test
+import java.util.concurrent.CountDownLatch
+
+import org.scalatest.{BeforeAndAfter, FunSuite}
+
+
+/**
+  * Here is a pair of classes in which a group of worker threads use two countdown latches:
+  * <ul>
+  * <li>The first is a start signal that prevents any worker from proceeding
+  * until the driver is ready for them to proceed;
+  * <li>The second is a completion signal that allows the driver to wait
+  * until all workers have completed.
+  * </ul>
+  */
+class CountDownLatch1Suite extends FunSuite with BeforeAndAfter {
+  private var counter = 0
+
+  before {
+    counter = 0
+  }
+
+  test("DriverWith0Threads") {
+    runDriver(0)
+    assertResult(2) { counter }
+  }
+
+  test("DriverWith1Threads") {
+    runDriver(1)
+    assertResult(12) { counter }
+  }
+
+  test("DriverWith2Threads") {
+    runDriver(2)
+    assertResult(22) { counter }
+  }
+
+  test("DriverWith10Threads(") {
+    runDriver(10)
+    ThreadUtil.sleep(20)
+    assertResult(102) { counter }
+  }
+
+  private def doSomethingElse() = {
+    System.out.println("Something else... counter=" + counter)
+    counter += 1
+  }
+
+  private def runDriver(numThreads: Int) = {
+    val startSignal = new CountDownLatch(1)
+    val doneSignal = new CountDownLatch(numThreads)
+    var i = 0
+    while ( {
+      i < numThreads
+    }) { // create and start threads
+      new Thread(new Worker(startSignal, doneSignal)).start()
+
+      {
+        i += 1; i
+      }
+    }
+    doSomethingElse() // don't let it run yet
+
+    startSignal.countDown() // let all threads proceed
+
+    doSomethingElse()
+    doneSignal.await() // wait for all to finish
+
+  }
+
+  private[solver] class Worker(val startSignal: CountDownLatch, val doneSignal: CountDownLatch) extends Runnable {
+    override def run(): Unit = {
+      try {
+        startSignal.await()
+        doWork()
+        doneSignal.countDown()
+      } catch {
+        case ex: InterruptedException =>
+      }
+    }
+
+    private[solver] def doWork() = {
+      System.out.println("Before working. counter=" + counter)
+      counter += 10
+      System.out.println("...  After working. counter=" + counter)
+    }
+  }
+
+}
---
+++ scala-test/com/barrybecker4/puzzle/common/solver/CountDownLatch2Suite.scala
@@ -0,0 +1,91 @@
+package com.barrybecker4.puzzle.common.solver
+
+import org.junit.Test
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.CyclicBarrier
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
+import java.util.concurrent.atomic.AtomicInteger
+
+import org.scalatest.{BeforeAndAfter, FunSuite}
+
+
+
+/**
+  * A typical usage is to divide a problem into N parts,
+  * describe each part with a Runnable that executes that portion and
+  * counts down on the latch, and queue all the Runnables to an Executor.
+  * When all sub-parts are complete, the coordinating thread will be able to pass through await.
+  * (When threads must repeatedly count down in this way, instead use a {@link CyclicBarrier}.)
+  */
+class CountDownLatch2Suite extends FunSuite with BeforeAndAfter {
+  private var counter: AtomicInteger = _
+
+  before {
+    counter =  new AtomicInteger(0)
+  }
+
+  test("DriverWith1Threads") {
+    runDriver(1)
+    assertResult(10) { counter.get }
+  }
+
+  test("DriverWith3Threads") {
+    runDriver(3)
+    assertResult(30) { counter.get }
+  }
+
+  /* fails for some odd reason when run from cmd line, but not in IDE */
+  test("DriverWith2Threads("){
+    runDriver(2)
+    assertResult(20) { counter.get }
+  }
+
+  test("DriverWith5Threads") {
+    runDriver(5)
+    assertResult(50) { counter.get }
+  }
+
+  test("DriverWith10Threads") {
+    runDriver(10)
+    assertResult(100) { counter.get }
+  }
+
+  test("DriverWith100Threads") {
+    runDriver(100)
+    assertResult(1000) { counter.get }
+  }
+
+  private def runDriver(numThreads: Int) = {
+    val doneSignal = new CountDownLatch(numThreads)
+    val e = Executors.newFixedThreadPool(numThreads)
+    // create and start threads
+    var i = 0
+    while ( {
+      i < numThreads
+    }) {
+      e.execute(new WorkerRunnable(doneSignal, i))
+
+      {
+        i += 1; i
+      }
+    }
+    // wait for all workers to finish
+    doneSignal.await()
+    System.out.println("Finished everything.")
+  }
+
+  private[solver] class WorkerRunnable(val doneSignal: CountDownLatch, val i: Int) extends Runnable {
+    override def run(): Unit = {
+      doWork(i)
+      doneSignal.countDown()
+    }
+
+    private[solver] def doWork(i: Int) = {
+      System.out.print("Before working on task " + i + ". counter=" + counter)
+      counter.addAndGet(10)
+      System.out.println("...  After working. counter=" + counter)
+    }
+  }
+
+}
