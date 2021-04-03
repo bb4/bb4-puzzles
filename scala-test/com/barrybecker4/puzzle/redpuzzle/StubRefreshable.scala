ce47da40a6ba5815cc95f2629d75cfba2071c287
Barry Becker <barrybecker4@gmail.com>
2021-04-03 05:29:20 -0700

    add jme3-plugins dependency

--- scala-test/com/barrybecker4/puzzle/redpuzzle/StubRefreshable.scala
+++ scala-test/com/barrybecker4/puzzle/redpuzzle/StubRefreshable.scala
@@ -8,6 +8,8 @@ class StubRefreshable extends Refreshable[PieceList, OrientedPiece] {
 
   override def refresh(state: PieceList, numTries: Long): Unit = {}
 
+  override def animateTransition(transition: OrientedPiece): PieceList = null
+
   override def finalRefresh(path: Option[Seq[OrientedPiece]],
                             state: Option[PieceList], numTries: Long, elapsedMillis: Long): Unit = {}
 }
